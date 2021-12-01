40d3f2605c1027a0b03a0c0892c86fe5478b9dd1
dwiddo <danwiddowson1@gmail.com>
2021-12-01 08:55:40 +0000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(r'.'))
+# sys.path.insert(0, os.path.abspath(r'.'))
 sys.path.insert(0, os.path.abspath(r'../..'))
 
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy',]
+MOCK_MODULES = ['numpy', 'scipy', 'ase', 'numba', 'h5py', 'ccdc']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
