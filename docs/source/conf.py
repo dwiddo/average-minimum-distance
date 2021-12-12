c16c2476764999b0a044a8c3d15072a2d96ecdd8
dwiddo <danwiddowson1@gmail.com>
2021-12-12 19:13:27 +0000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,9 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath(r'.'))
-sys.path.append(0, os.path.abspath(r'..'))
+sys.path.insert(0, os.path.abspath(r'..'))
 sys.path.insert(0, os.path.abspath(r'../..'))
+sys.path.insert(0, os.path.abspath(r'../amd'))
 
 import mock
  
