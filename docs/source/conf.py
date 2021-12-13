1d343c36604995f6d4e8690d2728625663222016
dwiddo <danwiddowson1@gmail.com>
2021-12-13 00:23:19 +0000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,7 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath(r'.'))
-# sys.path.insert(0, os.path.abspath(r'..'))
 sys.path.insert(0, os.path.abspath(r'../..'))
-# sys.path.insert(0, os.path.abspath(r'../amd'))
-# sys.path.insert(0, os.path.abspath(r'../../amd'))
 
 import mock
  
@@ -88,7 +84,7 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# # Add any paths that contain custom static files (such as style sheets) here,
+# # relative to this directory. They are copied after the builtin static files,
+# # so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
