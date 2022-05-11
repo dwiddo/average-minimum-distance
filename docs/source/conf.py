989be5dff424eb7bca5c299f01a52999cefe34c6
dwiddo <danwiddowson1@gmail.com>
2022-05-11 18:45:39 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,8 +82,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'sphinx_rtd_theme'
-default_dark_mode = True
+html_theme = 'sphinx_rtd_dark_mode'
+default_dark_mode = False
 
 html_sidebars = { 
     '**': [
@@ -97,4 +97,4 @@ html_sidebars = {
 # # Add any paths that contain custom static files (such as style sheets) here,
 # # relative to this directory. They are copied after the builtin static files,
 # # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../build/html/_static']
+# html_static_path = ['_static']
